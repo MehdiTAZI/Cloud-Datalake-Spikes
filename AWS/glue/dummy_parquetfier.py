ad25254b3c630f3e199e24b61ee5fd9c76e8bfd0
Mehdi TAZI <gtmehdi@gmail.com>
2019-05-23 17:09:58 +0200

    Update dummy_parquetfier.py

--- AWS/glue/dummy_parquetfier.py
+++ AWS/glue/dummy_parquetfier.py
@@ -5,7 +5,6 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 sc = SparkContext()
@@ -13,30 +12,16 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
-## @type: DataSource
-## @args: [database = "mydb", table_name = "articles", transformation_ctx = "datasource0"]
-## @return: datasource0
-## @inputs: []
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "mydb", table_name = "articles", transformation_ctx = "datasource0")
-## @type: ApplyMapping
-## @args: [mapping = [("article", "string", "article", "string"), ("price", "double", "price", "double"), ("quantity", "int", "quantity", "int"), ("comment", "string", "comment", "string"), ("date", "string", "date", "string")], transformation_ctx = "applymapping1"]
-## @return: applymapping1
-## @inputs: [frame = datasource0]
-applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("article", "string", "article", "string"), ("price", "double", "price", "double"), ("quantity", "int", "quantity", "int"), ("comment", "string", "comment", "string"), ("date", "string", "date", "string")], transformation_ctx = "applymapping1")
-## @type: ResolveChoice
-## @args: [choice = "make_struct", transformation_ctx = "resolvechoice2"]
-## @return: resolvechoice2
-## @inputs: [frame = applymapping1]
-resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_struct", transformation_ctx = "resolvechoice2")
-## @type: DropNullFields
-## @args: [transformation_ctx = "dropnullfields3"]
-## @return: dropnullfields3
-## @inputs: [frame = resolvechoice2]
-dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
-## @type: DataSink
-## @args: [connection_type = "s3", connection_options = {"path": "s3://bucketmehdi/articles
-"}, format = "parquet", transformation_ctx = "datasink4"]
-## @return: datasink4
-## @inputs: [frame = dropnullfields3]
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = dropnullfields3, connection_type = "s3", connection_options = {"path": "s3://bucketmehdi/articlesParquet"}, format = "parquet", transformation_ctx = "datasink4")
+
+
+dynamic_frame = glueContext.create_dynamic_frame.from_catalog(database = "mydb", table_name = "articles", transformation_ctx = "dynamic_frame")
+
+mapped_df = ApplyMapping.apply(frame = dynamic_frame, mappings = [("article", "string", "article", "string"), ("price", "double", "price", "double"), ("quantity", "int", "quantity", "int"), ("comment", "string", "comment", "string"), ("date", "string", "date", "string")], transformation_ctx = "mapped_df")
+
+resolvechoice = ResolveChoice.apply(frame = mapped_df, choice = "make_struct", transformation_ctx = "resolvechoice")
+
+dropnullfields = DropNullFields.apply(frame = resolvechoice, transformation_ctx = "dropnullfields")
+
+parquet_df = glueContext.write_dynamic_frame.from_options(frame = dropnullfields, connection_type = "s3", connection_options = {"path": "s3://bucketmehdi/articlesParquet"}, format = "parquet", transformation_ctx = "parquet_df")
+
 job.commit()
